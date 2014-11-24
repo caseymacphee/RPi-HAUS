7e53cc1ba5cb33e560ce61bb3118b4f455f85754
miracode <rascatim@gmail.com>
2014-11-24 11:24:14 -0800

    attempted to add login functional tests, will probably get changed around

--- haus_site/functional_tests/tests.py
+++ haus_site/functional_tests/tests.py
@@ -29,7 +29,28 @@ class NewVisitorTest(LiveServerTestCase):
 
         # as a visitor, i'd like to create an account
 
+
+class MemberTests(LiveServerTestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_member_login(self):
         # as a member, I'd like to log in to a secure account
+        ## This may get changed around for actual homepage functionality
+        username_box = self.browser.find_element_by_id('username')
+        password_box = self.browser.find_element_by_id('password')
+
+        username_box.send_keys('admin')
+        password_box.send_keys('admin')
+        password_box.send_keys(Keys.ENTER)
+
+        self.wait_for_element_with_id('logout')
+        navbar = self.browser.find_element_by_css_selector('.navbar')
+        self.assertIn('admin', navbar.text)
 
     def test_dashboard_functionality(self):
         pass
