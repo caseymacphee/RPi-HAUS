7ac1a3ed29de86a57d024f7793a4e6a61942f3f4
BFriedland <friedland.ben@gmail.com>
2014-11-25 12:03:54 -0800

    Added post support example code to API for devices

--- haus_site/api/serializers.py
+++ haus_site/api/serializers.py
@@ -16,8 +16,49 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
 
 class DeviceSerializer(serializers.Serializer):
     pk = serializers.Field()
-    name = serializers.CharField(max_length=100)
+    name = serializers.CharField(max_length=200)
     atoms = serializers.SerializerMethodField('get_atoms')
+
+    # NOTE: Devices currently only have one user.
+    # If this changes, see also the models.py file and views.py file.
+    # ForeignKey for serializers is RelatedField. Reference:
+    # http://www.django-rest-framework.org/api-guide/relations/
+    user = serializers.RelatedField()
+
+    # 'monitor' or 'controller'
+    device_type = serializers.CharField(max_length=20)
+
+    # serialpath might not be on Devices -- maybe move to AtomSerializer
+    # depending on what the model turns out to be?
+    serialpath = serializers.CharField(max_length=200)
 
     def get_atoms(self, obj):
         return [atom.name for atom in obj.atoms.all()]
+
+    # Requires importing the models (so you can create a new entry in the DB)
+
+    def restore_object(self, attrs, instance=None):
+
+        if instance:
+
+            instance.name = attrs.get('name', instance.name)
+            instance.serialpath = attrs.get('serialpath', instance.serialpath)
+            instance.user = attrs.get('user', instance.user)
+            instance.device_type = attrs.get('device_type', instance.device_type)
+            instance.save()
+            return instance
+
+        return Device(**attrs)
+
+
+
+
+
+
+
+
+
+
+
+
+
--- haus_site/api/views.py
+++ haus_site/api/views.py
@@ -7,6 +7,7 @@ from api.serializers import DeviceSerializer
 from rest_framework.response import Response
 from django.http import Http404
 from rest_framework import status
+from haus.models import Device, Atom
 
 
 class DeviceListView(APIView):
@@ -29,26 +30,28 @@ class DeviceDetailView(APIView):
     # NOTE: Unlike in the tutorial, request is passed to get_object()
     # because we're not currently importing models. We can import models
     # and change the query from request.....get() to a more direct model query.
-    def get_object(self, request, device_pk):
+    def get_object(self, device_pk):
         try:
             # This is based on the assumption that users can only see
             # devices that include those users as owners. (nb reverse relation)
             # Should this assumption be changed, this code must change as well:
-            return request.user.devices.get(pk=device_pk)
-        # The tutorial recommended specifically excepting on their model's
-        # DoesNotExist class (which our model would have too, if it was
-        # imported here, which is isn't right now).
-        except:
+            # return request.user.devices.get(pk=device_pk)
+
+            # Now changed to models and open-access:
+            return Device.objects.get(pk=device_pk)
+
+        except Device.DoesNotExist:
             raise Http404
 
     def get(self, request, device_pk, format=None):
-        device = self.get_object(request, device_pk)
+        device = self.get_object(device_pk)
         serializer = DeviceSerializer(device)
         return Response(serializer.data)
 
     def put(self, request, device_pk, format=None):
-        device = self.get_object(request, device_pk)
+        device = self.get_object(device_pk)
         serializer = DeviceSerializer(device, data=request.DATA)
+        print(str(serializer))
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
