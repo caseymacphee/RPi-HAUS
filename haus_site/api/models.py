9522882cf371441e5b7a10adf6d9b4a6a8df2108
jbbrokaw <jbrokaw@gmail.com>
2014-11-25 10:17:46 -0800

    Cleans up test model in api app

--- haus_site/api/models.py
+++ haus_site/api/models.py
@@ -1,12 +1,3 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
-
-
-# class TimeSeries(models.Model):
-#     owner = models.ForeignKey(User, verbose_name="Owner of data",
-#                               related_name='data', blank=True, null=True)
-#     timestamp = models.DateTimeField("Time data point recorded", blank=True,
-#                                      null=True)
-#     value = models.DecimalField(max_digits=6, decimal_places=3,
-#                                 verbose_name="Data point value")
