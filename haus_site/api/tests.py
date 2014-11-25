5acbf3e1d127224a82e0274020963e3c9657deb5
jbbrokaw <jbrokaw@gmail.com>
2014-11-25 09:56:54 -0800

    Adds a working test of /devices/ route

--- haus_site/api/tests.py
+++ haus_site/api/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase
-
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
 # Create your tests here.
+
+from haus.models import Device
+
+
+class DeviceAPITests(TestCase):
+    def setUp(self):
+        superuser = User.objects.create_superuser("admin", "", "admin")
+        Device.objects.create(name="testdevice", user=superuser, serialpath='',
+                              device_type="monitor")
+
+    def test_existing_device_is_retrieved(self):
+        """
+        was_published_recently() should return False for questions whose
+        pub_date is in the future
+        """
+        client = Client()
+        client.login(username="admin", password="admin")
+        response = client.get('/devices/')
+        # raise Exception(str(dir(response)))
+        self.assertIn("testdevice", response.content)
