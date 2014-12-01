9fdf09dc7dcc39eb55f06f375e98586e1bfbfda1
Charlie Rode <chrode10@gmail.com>
2014-12-01 11:40:57 -0800

    Update settings to set credentials

--- haus_site/haus_site/settings.py
+++ haus_site/haus_site/settings.py
@@ -10,6 +10,8 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+from credentials import set_credentials
+set_credentials()
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
