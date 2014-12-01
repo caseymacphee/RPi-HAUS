ac9f1b190cce60044846bc4fc72271e517e22613
Mark Ableidinger <mark.ableidinger@gmail.com>
2014-12-01 15:08:32 -0800

    templates in active user's name to dataview

--- haus_site/haus/views.py
+++ haus_site/haus/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-
+from django.template import RequestContext, loader
 
 # Create your views here.
 
+
 @login_required
 def home(request):
-    return render(request, 'haus/dataview.html', None)
+    context = RequestContext(request, {
+        'user': request.user
+    })
+    template = loader.get_template('haus/dataview.html')
+    return HttpResponse(template.render(context))
